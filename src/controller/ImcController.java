cf57669bf58210baaefb61036ddf092d9d2baeef
OlivierBillant <99239307+OlivierBillant@users.noreply.github.com>
2022-03-23 16:35:58 +0100

    commit MVC pas fait

--- src/controller/ImcController.java
+++ src/controller/ImcController.java
@@ -2,7 +2,7 @@ package controller;
 
 import view.Fenetre;
 
-public class ViewController {
+public class ImcController {
 
 	public static void main(String[] args) {
 		Fenetre fenetre = new Fenetre();
