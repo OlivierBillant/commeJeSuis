a993dda562221d77d119ef62e8ececf64b99e594
OlivierBillant <99239307+OlivierBillant@users.noreply.github.com>
2022-03-23 11:50:15 +0100

    Programme fonctionnel

---
+++ src/controller/ViewController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import view.Fenetre;
+
+public class ViewController {
+
+	public static void main(String[] args) {
+		Fenetre fenetre = new Fenetre();
+
+	}
+
+}
---
+++ src/view/Fenetre.java
@@ -0,0 +1,84 @@
+	package view;
+
+	import java.awt.event.ActionEvent;
+	import java.awt.event.ActionListener;
+
+	import javax.swing.JButton;
+	import javax.swing.JFrame;
+	import javax.swing.JLabel;
+	import javax.swing.JPanel;
+	import javax.swing.JTextField;
+
+	public class Fenetre extends JFrame {
+		
+		public Fenetre() {
+			setDefaultCloseOperation(DISPOSE_ON_CLOSE); //libérer l'espace mémoire
+			//Les variables de notre fenetre : x, y, w; h valeurs en px
+			setBounds(150,150,400,300);
+			setTitle("Fenetre 1");
+			
+			JPanel monPanel = new JPanel();
+			//Associer le panel à la fenêtre
+			setContentPane(monPanel);
+			setLayout(null);
+			
+			//Champ texte : JLabel
+			JLabel monLabel_1 = new JLabel();
+			monLabel_1.setText("Votre poids en kg : ");
+			monLabel_1.setBounds(20, 20, 200, 20);
+			//ajouter au panel
+			monPanel.add(monLabel_1);
+			
+			//Champ de saisie
+			JTextField saisie_1 = new JTextField();
+			saisie_1.setBounds(20, 40, 100, 20);
+			monPanel.add(saisie_1);
+			
+			//Champ texte : JLabel
+			JLabel monLabel_2 = new JLabel();
+			monLabel_2.setText("Votre taille en m : ");
+			monLabel_2.setBounds(150, 20, 200, 20);
+			//ajouter au panel
+			monPanel.add(monLabel_2);
+			
+			//Champ de saisie
+			JTextField saisie_2 = new JTextField();
+			saisie_2.setBounds(150, 40, 100, 20);
+			monPanel.add(saisie_2);
+			
+			//Le trigger / Bouton
+			JButton benjamin = new JButton("Calculez votre IMC");
+			benjamin.setBounds(20, 80, 230, 40);
+			monPanel.add(benjamin);
+			
+			JLabel monLabel_3 = new JLabel();
+			monLabel_3.setText("Affichage IMC");
+			monLabel_3.setBounds(20, 130, 230, 20);
+			//ajouter au panel
+			monPanel.add(monLabel_3);
+			
+			setVisible(true); // rendre la fenetre visible
+			
+			benjamin.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					String poids_str = saisie_1.getText();
+					String taille_str = saisie_2.getText();
+					float poids = Float.parseFloat(poids_str);
+					float taille = Float.parseFloat(taille_str); 
+					float imc = poids/taille/taille;
+					String imc_str = String.format("%.2f",imc);
+						if(imc < 18) {
+							monLabel_3.setText(imc_str+" - Vous êtes maigre");
+						}
+						else if(imc < 25) {
+							monLabel_3.setText(imc_str+" - Vous êtes en bonne santé");
+						}
+						else {
+							monLabel_3.setText(imc_str+" - Vous êtes en surpoids");
+						}
+				}
+			});
+		}
+	}
+
